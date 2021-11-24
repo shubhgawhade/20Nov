1c342b57e3f11abb162de7026c48ffaa18a391e7
Shubh Gawhade <51043720+shubhgawhade@users.noreply.github.com>
2021-11-24 12:28:31 +0530

    Initial commit

---
+++ Assets/Movement.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public Transform[] goal;
+    public float speed;
+    public float stopDistance;
+    public int goalNum;
+
+    private void LateUpdate()
+    {
+        if(goalNum < goal.Length)
+        {
+            Vector3 dir = goal[goalNum].transform.position - transform.position;
+
+            if (dir.magnitude > stopDistance)
+            {
+                transform.LookAt(new Vector3(dir.x, transform.position.y, dir.z));
+                transform.Translate(dir.normalized * speed * Time.deltaTime, Space.World);
+            }
+            else
+            {
+                goalNum++;
+            }
+        }
+    }
+}
